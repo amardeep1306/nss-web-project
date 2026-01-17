abf9cbe1b2f4ac3a6f0e2ba2ac087fb7844e5735
amardeep <amardeepkumar13641364@gmail.com>
2026-01-18 04:22:14 +0530

    Fixed secure false for port 587

--- server/index.js
+++ server/index.js
@@ -134,7 +134,7 @@ app.post('/api/auth/send-otp', async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",  
       port: 587,              
-      secure: true,            
+      secure: false,            
       auth: { 
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS 
