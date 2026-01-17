29372abb898ba8fce3b9673fdd8007affc8e4ee6
amardeep <amardeepkumar13641364@gmail.com>
2026-01-18 05:16:00 +0530

    Switched to service: gmail

--- server/index.js
+++ server/index.js
@@ -138,10 +138,7 @@ app.post('/api/auth/send-otp', async (req, res) => {
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS 
       },
-      tls: {
-        rejectUnauthorized: false
-      },
-      family: 4
+      family: 4 
     });
     console.log("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL");
     const mailOptions = {
